321344c887c5ae93a87a77a5bbcd0be30c1576ec
cloudxpc <cloudxpc@126.com>
2017-06-30 17:04:22 +0800

    Add controller for test

--- src/main/java/com/ricktech/relation/controllers/UserController.java
+++ src/main/java/com/ricktech/relation/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.ricktech.relation.controller;
+package com.ricktech.relation.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
